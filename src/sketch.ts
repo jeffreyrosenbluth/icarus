0feec4ef12fdb7ab2a3b77d12dcad97206f46148
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2023-07-25 20:06:16 -0400

    tweak relative sizes

--- src/sketch.ts
+++ src/sketch.ts
@@ -304,7 +304,7 @@ function draw() {
 function resizeCanvas() {
     if (!canvas || !ctx) { return };
     const windowWidth = 0.75 * window.innerWidth;
-    const windowHeight = 0.75 * window.innerHeight;
+    const windowHeight = 0.60 * window.innerHeight;
 
     canvas.width = Math.floor(windowWidth * window.devicePixelRatio);
     canvas.height = Math.floor(windowHeight * window.devicePixelRatio);
